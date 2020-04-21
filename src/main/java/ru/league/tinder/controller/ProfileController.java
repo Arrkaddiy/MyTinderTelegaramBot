d81ce8eb4c16ae9dfd70a2efa3f31b7931773f61
ARK <Arrkaddiy@gmail.com>
2020-04-21 23:17:54 +0300

    Переработка проекта от 21 04 Перенос логиги из контролера в сервис

--- src/main/java/ru/league/tinder/controller/ProfileController.java
+++ src/main/java/ru/league/tinder/controller/ProfileController.java
@@ -1,7 +1,5 @@
 package ru.league.tinder.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.league.tinder.model.ProfileModel;
@@ -10,8 +8,6 @@ import ru.league.tinder.service.ProfileService;
 @RestController
 @RequestMapping("tinder/user/{tinderId}/profiles")
 public class ProfileController {
-
-    private static final Logger log = LoggerFactory.getLogger(UserProfileController.class);
 
     @Autowired
     private ProfileService profileService;
